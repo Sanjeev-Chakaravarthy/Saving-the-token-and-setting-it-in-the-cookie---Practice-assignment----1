2468f90a6a28f476aed6836f2c16d8d76f888aea
Sanjeev-Chakaravarthy <sanjeev.ms28122@gmail.com>
2025-03-20 09:39:26 +0530

    assignment completed

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
+const jwt = require('jsonwebtoken')
+const cryptojs = require('crypto-js')
+require('dotenv').config()
+
+const SECRET_KEY = process.env.SECRET_KEY
+const ENCRYPTION_KEY = process.env.ENCRYPTION_KEY
+
+
 const encrypt = (payload) => {
   // encrypt the payload and return token
+  const tok = jwt.sign(payload, SECRET_KEY, {expiresIn: '30m'})
+
+  const encryptedTok = cryptojs.AES.encrypt(tok,ENCRYPTION_KEY).toString()
+  return encryptedTok
 }
 
 const decrypt = (token) => {
   // return decoded payload
+  const bytes = CryptoJS.AES.decrypt(encryptedToken, ENCRYPTION_KEY)
+  const decryptedTok = bytes.toString(CryptoJS.enc.Utf8)
+
+
+  return jwt.verify(decryptedTok,SECRET_KEY)
 }
 
 module.exports = {
